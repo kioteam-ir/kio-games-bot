60cd549875ff9067596c837ddd237b1404e76033
mamahoos <m4m4hoos@gmail.com>
2025-08-12 17:32:20 +0330

    Refactor: implement async task manager with strategies for task execution and management

--- 4fall_game_bot/bot/bot.py
+++ 4fall_game_bot/bot/bot.py
@@ -5,8 +5,8 @@ from .config import config
 bot = Client(
     name      = config.SESSION_NAME,
     bot_token = config.TOKEN,
-    api_hash  = config.API_HASH or None,
-    api_id    = config.API_ID   or None,
+    api_hash  = config.API_HASH,
+    api_id    = config.API_ID,
 )
 
 __all__ = ("bot",)      # Only can import 'bot' name-space from this file
