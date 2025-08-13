c1a581c037d2102623c03430188bb97cac94ce87
yas3in <jastinafa2016@gmail.com>
2025-08-13 23:35:29 +0330

    edit and add models

--- 4fall_game_bot/db/app/models.py
+++ 4fall_game_bot/db/app/models.py
@@ -15,16 +15,29 @@ class Admin(AbstractUser):
 class User(models.Model):
     id = models.BigIntegerField(primary_key=True)
     join_time = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
 
 
 class Game(models.Model):
-    winner = models.ForeignKey(User, related_name="winner", on_delete=models.CASCADE)
-    loser = models.ForeignKey(User, related_name="loser", on_delete=models.CASCADE)
+    WINNER_CHOICES = [
+        ('player_1', 'player_1'),
+        ('player_2', 'player_2'),
+        ('draw', 'Draw'),
+    ]
+
+    player_1 = models.ForeignKey(User, related_name="player_1", on_delete=models.CASCADE, null=True)
+    player_2 = models.ForeignKey(User, related_name="player_2", on_delete=models.CASCADE, null=True)
     type = models.IntegerField()
+    
+    result = models.CharField(max_length=10, choices=WINNER_CHOICES)
 
 
 class Score(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="score")
     games = models.IntegerField()
     wins = models.IntegerField()
-    
+
+
+class Command(models.Model):
+    command = models.CharField(max_length=256, primary_key=True)
+    value = models.CharField(max_length=580)
