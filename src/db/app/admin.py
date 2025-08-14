e70a0a61415b884f87e1c270136e2060700c5bc5
yas3in <jastinafa2016@gmail.com>
2025-08-14 23:42:41 +0330

    feat: add all models to panel

--- src/db/app/admin.py
+++ src/db/app/admin.py
@@ -1,9 +1,24 @@
 from django.contrib.admin import register
 from django.contrib import admin
 
-from app.models import User
+from app.models import User, Command, Game, Score
 
 
 @register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("id", "is_superuser", "is_banned", "joined_time")
+
+
+@register(Command)
+class CommandAdmin(admin.ModelAdmin):
+    list_display = ("command", "value")
+
+
+@register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ("result", "player_1", "player_2", "type")
+
+
+@register(Score)
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ("user", "wins", "games")
