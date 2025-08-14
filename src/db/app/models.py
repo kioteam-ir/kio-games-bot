266b4211c83cc07b6cb77ea9de4f155f45bb5f6f
yas3in <jastinafa2016@gmail.com>
2025-08-14 21:21:14 +0330

    check

---
+++ src/db/app/models.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+class Admin(AbstractUser):
+
+    REQUIRED_FIELDS = []
+    
+    class Meta:
+        verbose_name = "Admin"
+        verbose_name_plural = "Admins"
+
+
+class User(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    join_time = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+
+
+class Game(models.Model):
+    WINNER_CHOICES = [
+        ('player_1', 'player_1'),
+        ('player_2', 'player_2'),
+        ('draw', 'Draw'),
+    ]
+
+    player_1 = models.ForeignKey(User, related_name="player_1", on_delete=models.CASCADE, null=True)
+    player_2 = models.ForeignKey(User, related_name="player_2", on_delete=models.CASCADE, null=True)
+    type = models.IntegerField()
+    
+    result = models.CharField(max_length=10, choices=WINNER_CHOICES)
+
+
+class Score(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="score")
+    games = models.IntegerField()
+    wins = models.IntegerField()
+
+
+class Command(models.Model):
+    command = models.CharField(max_length=256, primary_key=True)
+    value = models.CharField(max_length=580)
