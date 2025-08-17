6798f907d5c05a3f40c7c33b11100b57108de2d0
mamahoos <m4m4hoos@gmail.com>
2025-08-18 00:55:56 +0330

    feat: add as_symbol to Cell type

--- src/core/game_engine/types.py
+++ src/core/game_engine/types.py
@@ -14,7 +14,12 @@ class Color(StrEnum):
     RED    = '🔴'
     BLUE   = '🔵'
     YELLOW = '🟡'
-    white  = '⬜️'
+    WHITE  = '⬜️'
+    
+class Symbol(StrEnum):
+    X     = "❌"
+    O     = "⭕️"
+    EMPTY = '⬜️'
 
 class Player(IntEnum):
     ONE = 1
@@ -29,8 +34,8 @@ class Player(IntEnum):
         color_mapping = (Color.GREEN, Color.BLUE)
         return color_mapping[self.value - 1]
     @property
-    def as_symbol(self) -> str:
-        symbol_mapping = ('X', 'O')
+    def as_symbol(self) -> Symbol:
+        symbol_mapping = (Symbol.X, Symbol.O)
         return symbol_mapping[self.value - 1]
 
     @property
@@ -54,9 +59,16 @@ class Cell(IntEnum):
     @property
     def as_color(self) -> Color:
         if self.is_empty:
-            return Color.white
+            return Color.WHITE
         player = self.as_player
         return player.as_color
+    
+    @property
+    def as_symbol(self) -> Symbol:
+        if self.is_empty:
+            return Symbol.EMPTY
+        player = self.as_player
+        return player.as_symbol
         
 
 BoardMatrix = List[List[Cell]]
