6129063cc7f66c8806150c2bd586d0a4d10660d8
yas3in <jastinafa2016@gmail.com>
2025-08-29 19:37:11 +0330

    edit migrations

--- src/app/migrations/0012_localizedtext_id_alter_localizedtext_key.py
+++ src/app/migrations/0012_localizedtext_id_alter_localizedtext_key.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='localizedtext',
             name='id',
-            field=models.BigAutoField(auto_created=True, default=1, primary_key=True, serialize=False, verbose_name='ID'),
+            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
             preserve_default=False,
         ),
         migrations.AlterField(
