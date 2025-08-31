3a9a73adcc94f6a55ceb62d750ddca8e64db511e
yas3in <jastinafa2016@gmail.com>
2025-08-31 16:05:48 +0330

    del: LocalizedText

--- src/app/models.py
+++ src/app/models.py
@@ -220,23 +220,6 @@ class Score(models.Model):
         instance.save()
 
 
-# class LocalizedText(models.Model):
-#     key = models.IntegerField()
-#     value = models.TextField(max_length=256)
-#     lang_code = models.CharField(max_length=2, default="fa")
-
-#     @staticmethod
-#     def get_text(lang_code):
-#         text_list = []
-#         instance = LocalizedText.objects.filter(lang_code=lang_code).values("key", "value")
-#         for row in instance:
-#             text_list.append(row)
-#         return text_list
-    
-#     def __str__(self):
-#         return f"{self.key} - {self.value}" 
-    
-
 class Sponser(models.Model):
     id = models.BigIntegerField(primary_key=True)
     link = models.CharField(max_length=255)
