f8ad6a7f246cf37fd61c430603849b9c0e785d0d
mamahoos <m4m4hoos@gmail.com>
2026-06-15 04:02:19 +0330

    refactor: remove Postgres wait logic from entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-echo "Waiting for Postgres..."
-until nc -z db 5432; do
-  sleep 1
-done
-echo "Postgres is up!"
-
 compile_locales() {
     echo "Compiling locales..."
     poetry run pybabel compile -d bot/locales -D bot
